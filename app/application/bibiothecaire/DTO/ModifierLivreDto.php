96ba0c1f2ddf1d3fb0007b1a7cb97e1dc50d1e96
obame-sketch <esaitondaobame08@gmail.com>
2026-06-16 12:07:03 +0200

    réalisation des dto

---
+++ app/application/bibiothecaire/DTO/ModifierLivreDto.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\DTO\Livre;
+
+/**
+ * DTO contenant les modifications d'un livre (tous les champs sont optionnels).
+ */
+final readonly class ModifierLivreDto
+{
+    /**
+     * L'utilisation de types nullables permet de ne modifier que les propriétés
+     * transmises lors de la requête.
+     */
+    public function __construct(
+        public ?string $titre = null,
+        public ?string $auteur = null,
+        public ?string $isbn = null,
+        public ?\DateTimeImmutable $datePublication = null,
+    ) {}
+}
+
+?>
