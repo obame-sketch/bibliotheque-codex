521f8882017e36f48fdbda42d648b670f6d2cadb
lmlouis <louiswora1@gmail.com>
2026-07-07 11:02:16 +0100

    fix: violation style pint

--- app/Infrastructure/Persistence/Eloquent/Models/BibliothequeModel.php
+++ app/Infrastructure/Persistence/Eloquent/Models/BibliothequeModel.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence\Eloquent\Models;
 
-use App\Domain\Bibliotheque\Bibliotheque;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
--- app/Infrastructure/Persistence/Eloquent/Models/LivreModel.php
+++ app/Infrastructure/Persistence/Eloquent/Models/LivreModel.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence\Eloquent\Models;
 
-use App\Domain\Bibliothecaire\Bibliothecaire;
-use App\Domain\Bibliotheque\Bibliotheque;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
